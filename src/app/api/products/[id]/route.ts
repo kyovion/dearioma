a9a576312caa21ef3574d5296db9ad173abc6b04
oky <oky.project.mail@gmail.com>
2026-02-23 23:42:49 +0700

    Add notes about new params in next.js 15

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,10 +1,14 @@
 import db from "@/src/lib/db";
 
+//old code need change to https://nextjs.org/docs/app/guides/upgrading/version-15#params--searchparams
+// export async function GET( req: Request, {params}: { params: { id: string } }) 
 export async function GET( req: Request, context: { params: Promise<{ id: string }> }) 
 {
   const { id } = await context.params
 
   const product = await db.product.findUnique({
+    //old code
+    // where: { id: params.id }
     where: { id }
   })
 
